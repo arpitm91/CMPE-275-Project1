5d7c62b5814cef58d3e6f9202f6de06ab5750464
arpitm91 <32207496+arpitm91@users.noreply.github.com>
2018-10-29 17:54:12 -0700

    Fileinfo Integration added

---
+++ integration/ask_table.py
@@ -0,0 +1,20 @@
+import pprint
+import grpc
+import file_transfer_pb2 as file_transfer
+import file_transfer_pb2_grpc as rpc
+
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = rpc.DataTransferServiceStub(channel)
+
+        request = file_transfer.FileInfo()
+        request.fileid = "file1"
+
+        response = stub.GetFileLocation(request)
+    print("Response received: ")
+    pprint.pprint(response)
+
+
+if __name__ == '__main__':
+    run()
