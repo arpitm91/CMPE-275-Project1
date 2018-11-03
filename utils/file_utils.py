b653622206944a9b21c7f80d4aa0d4be39af8cbd
Aartee Kasliwal <aarteekasliwal@gmail.com>
2018-11-02 21:28:12 -0700

    Merge chunks

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,5 +1,6 @@
 import os
 import math
+import glob
 
 SEQ_SIZE = 1024 * 1024  # 1MB
 
@@ -17,8 +18,25 @@ def get_file_seqs(filename):
             yield piece
 
 
-def write_file_chunks(message):
-    os.mkdir(message.fileName)
-    file_name = message.fileName + "/" + str(message.chunkId)
+def write_file_chunks(message, folder):
+    try:
+        os.mkdir(os.path.join(folder, message.fileName))
+    except OSError as e:
+        if e.errno != os.errno.EEXIST:
+            raise
+        pass
+    file_name = os.path.join(folder, message.fileName, str(message.chunkId))
     with open(file_name, "ab") as myfile:
         myfile.write(message.data)
+
+#
+def merge_chunks(file_name_folder):
+    pass
+#     merged_file_name = "merged_file"
+#     with open(merged_file_name, "ab") as merged_file:
+#         for chunks in glob.glob(file_name_folder):
+#             print("chunk: ", chunks)
+#             # for chunks in os.listdir(file_name_folder):
+#             with open(chunks, "rb") as chunk_file:
+#                 merged_file.write(str.encode(chunk_file.read()))
+#         print(chunks)
