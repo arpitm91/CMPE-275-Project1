8c3cd244ba7b0a548b372653dbba081dbf2f5a28
arpitm91 <32207496+arpitm91@users.noreply.github.com>
2018-11-11 17:36:24 -0800

    dc_info ip added

--- data_center/configs/data_center_info.py
+++ data_center/configs/data_center_info.py
@@ -1,29 +1,36 @@
 data_center = {
     "dc_aartee_1": {
+        "ip": "10.0.1.3",
         "port": "10001",
         "folder": "/Users/aarteekasliwal/Desktop/DataCenter/1"
     },
     "dc_aartee_2": {
+        "ip": "10.0.1.3",
         "port": "10002",
         "folder": "/Users/aarteekasliwal/Desktop/DataCenter/2"
     },
     "dc_aartee_3": {
+        "ip": "10.0.1.3",
         "port": "10003",
         "folder": "/Users/aarteekasliwal/Desktop/DataCenter/3"
     },
     "dc_aartee_4": {
+        "ip": "10.0.1.3",
         "port": "10004",
         "folder": "/Users/aarteekasliwal/Desktop/DataCenter/4"
     },
     "dc_3": {
+        "ip": "10.0.1.1",
         "port": "10012",
         "folder": "/Users/arpit/Desktop/DataCenter/DC1"
     },
     "dc_4": {
+        "ip": "10.0.1.2",
         "port": "10001",
         "folder": "/Users/anujchaudhari/Desktop/CMPE275/DataCenter/0"
     },
     "dc_5": {
+        "ip": "10.0.1.2",
         "port": "12000",
         "folder": "/Users/anujchaudhari/Desktop/CMPE275/DataCenter/0"
     }
