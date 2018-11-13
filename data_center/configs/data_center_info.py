e6adab5c811fe0c07c7b2b79b295cdc0abb877c6
Anuj Chaudhari <chaudharianuj93@gmail.com>
2018-11-12 18:48:28 -0800

    update raft and dc configs

--- data_center/configs/data_center_info.py
+++ data_center/configs/data_center_info.py
@@ -1,37 +1,37 @@
 data_center = {
     "dc_aartee_1": {
-        "ip": "10.0.1.3",
-        "port": "10001",
+        "ip": "10.0.10.3",
+        "port": "11001",
         "folder": "/Users/aarteekasliwal/Desktop/DataCenter/1"
     },
     "dc_aartee_2": {
-        "ip": "10.0.1.3",
-        "port": "10002",
+        "ip": "10.0.10.3",
+        "port": "11002",
         "folder": "/Users/aarteekasliwal/Desktop/DataCenter/2"
     },
     "dc_aartee_3": {
-        "ip": "10.0.1.3",
-        "port": "10003",
+        "ip": "10.0.10.3",
+        "port": "11003",
         "folder": "/Users/aarteekasliwal/Desktop/DataCenter/3"
     },
     "dc_aartee_4": {
-        "ip": "10.0.1.3",
-        "port": "10004",
+        "ip": "10.0.10.3",
+        "port": "11004",
         "folder": "/Users/aarteekasliwal/Desktop/DataCenter/4"
     },
     "dc_3": {
-        "ip": "10.0.1.1",
-        "port": "10012",
+        "ip": "10.0.10.1",
+        "port": "11012",
         "folder": "/Users/arpit/Desktop/DataCenter/DC1"
     },
     "dc_4": {
         "ip": "localhost",
-        "port": "12000",
+        "port": "11000",
         "folder": "/Users/anujchaudhari/Desktop/CMPE275/DataCenter/0"
     },
     "dc_5": {
         "ip": "localhost",
-        "port": "12001",
+        "port": "11001",
         "folder": "/Users/anujchaudhari/Desktop/CMPE275/DataCenter/1"
     }
 }
